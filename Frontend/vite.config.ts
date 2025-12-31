e318b676f3a799a17419d660342ccac632ac1e55
Paras Limbu <paras078@academiacollege.edu.np>
2025-12-31 14:31:11 +0545

    fix: update API proxy target to use environment variable

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
+
 export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: process.env.API,
         changeOrigin: true,
       }
     },
